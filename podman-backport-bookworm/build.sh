8e86614f35255a1fab11920b58a549af4d194104
angelsl <angelsl@in04.sg>
2024-01-21 02:41:22 +0800

    Use go from backports

--- podman-backport-bookworm/build.sh
+++ podman-backport-bookworm/build.sh
@@ -11,9 +11,10 @@ DEBIAN_REV=350b6e77e2267beb83b76abbb5f847c8f969c4a5
 mkdir -p /etc/apt/sources.list.d
 cat <<EOF > /etc/apt/sources.list.d/src.list
 deb-src http://deb.debian.org/debian testing main
+deb http://deb.debian.org/debian bookworm-backports main
 EOF
 apt-get update -qy
-apt-get install -qy packaging-dev debian-keyring devscripts equivs curl
+apt-get install -qy packaging-dev debian-keyring devscripts equivs curl golang-go/bookworm-backports
 
 git clone --depth=1 -b $TAG https://github.com/containers/podman.git podman
 curl "https://salsa.debian.org/debian/libpod/-/archive/${DEBIAN_REV}/libpod-${DEBIAN_REV}.tar.gz?path=debian" | tar -C podman --strip-components=1 -zxv
