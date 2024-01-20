6ac395e6d7174cd978d7ffacc5f618ff880051ba
angelsl <angelsl@in04.sg>
2024-01-21 01:23:37 +0800

    Build more deps

--- podman-backport-bookworm/build.sh
+++ podman-backport-bookworm/build.sh
@@ -20,10 +20,18 @@ dopkg() {
   popd
 }
 
-echo 'deb-src http://deb.debian.org/debian/ testing main' >> /etc/apt/sources.list
+mkdir -p /etc/apt/sources.list.d
+cat <<EOF > /etc/apt/sources.list.d/src.list
+deb-src http://deb.debian.org/debian testing main
+EOF
 apt-get update -qy
 apt-get install -qy packaging-dev debian-keyring devscripts equivs
 
+dopkg golang-github-containers-storage
+dopkg golang-github-container-orchestrated-devices-container-device-interface
+dopkg golang-github-containers-buildah-dev
+
 dopkg golang-github-checkpoint-restore-go-criu
 dopkg golang-github-checkpoint-restore-checkpointctl
+
 dopkg libpod
